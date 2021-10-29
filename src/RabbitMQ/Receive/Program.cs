17956929a2a56a7a489de795d908162ac917ccf1
shaippinho <shaienepardo@gmail.com>
2021-10-28 22:30:40 -0300

    Commit tutorial RabbitMQ

---
+++ src/RabbitMQ/Receive/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+var consumidor = new EventingBasicConsumer(channel);
+
+consumidor.Received += (modelo, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("[x] Recebido {0}", message);
+};
+
+channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumidor);
+
+
+Console.WriteLine("Pressione [enter] para sair.");
+Console.ReadLine();
+
+
---
+++ src/RabbitMQ/Send/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+
+int i = 0;
+for (; ; )
+{
+    i++;
+    string message = "Mensagem número " + i;
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine("[x] Enviado {0}", message);
+
+    System.Threading.Thread.Sleep(1000);
+}
+
+Console.WriteLine("Pressione [enter] para sair.");
+Console.ReadLine();
+
+
